f5fe53e8737c1eb54fb0727dd8ac1549653aa489
Mayank-Seth <sethmayank296@gmail.com>
2022-03-15 14:44:58 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component, VERSION } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
+// formgroup is control complete form
+// formcomtrol it handle single input field
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  loginForm = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  loginUser() {
+    console.log(this.loginForm.value);
+  }
 }
